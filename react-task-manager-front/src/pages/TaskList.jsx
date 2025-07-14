7505ddc61b4e7c889c2ec05148fb3d4c8a189e2c
antonella1986 <antonella.leo.mail@gmail.com>
2025-07-14 12:07:47 +0200

    implement tasks list with a table format

--- react-task-manager-front/src/pages/TaskList.jsx
+++ react-task-manager-front/src/pages/TaskList.jsx
@@ -1,10 +1,36 @@
 import { NavLink } from "react-router-dom";
+import { GlobalContext } from "../contexts/GlobalContext";
+import { useContext } from "react";
 
 export function TaskList() {
+    const { tasks } = useContext(GlobalContext);
+
     return (
         <div>
             <NavLink to="/">Homepage</NavLink>
             <h1>Task list</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Stato</th>
+                        <th>Data di creazione</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tasks.map(task => (
+                        <tr key={task.id}>
+                            <td>{task.title}</td>
+                            <td>{task.status}</td>
+                            <td>{new Date(task.createdAt).toLocaleDateString('it-IT', {
+                                day: '2-digit',
+                                month: '2-digit',
+                                year: 'numeric'
+                            })}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
