698c0be44bc59f489ba0e85466532dc2b043016c
antonella1986 <antonella.leo.mail@gmail.com>
2025-07-14 15:49:08 +0200

    update context and add comments for study purposes

--- react-task-manager-front/src/contexts/GlobalContext.jsx
+++ react-task-manager-front/src/contexts/GlobalContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext } from "react";
 import { useTasks } from "../hooks/useTasks";
 
 const GlobalContext = createContext();
--- react-task-manager-front/src/hooks/useTasks.jsx
+++ react-task-manager-front/src/hooks/useTasks.jsx
@@ -7,8 +7,10 @@ export function useTasks() {
         async function fetchTasks() {
             const response = await fetch("http://localhost:3001/tasks");
             const data = await response.json();
+            //i dati ricevuti li metto nello stato tasks tramite setTasks(data)
             setTasks(data);
         }
+        //avvio davvero la richiesta (altrimenti la funzione non sarebbe mai eseguita, ma solo dichiarata, e i task non verrebbero mai caricati nello stato)
         fetchTasks();
     }, []);
 
@@ -20,15 +22,16 @@ export function useTasks() {
             status: "todo",
             createdAt: new Date().toISOString(),
         };
+        //aggiorno lo stato tasks aggiungendo newTask alla fine dell'array esistente
         setTasks([...tasks, newTask]);
     }
 
     const removeTask = (taskId) => {
-        setTasks(tasks.filter((task) => task.id !== taskId));
+        setTasks(tasks.filter(task => task.id !== taskId));
     }
 
     const updateTask = (taskId, updatedTask) => {
-        setTasks(tasks.map((task) => task.id === taskId ? updatedTask : task));
+        setTasks(tasks.map(task => task.id === taskId ? updatedTask : task));
     }
 
     return { tasks, setTasks, addTask, removeTask, updateTask };
